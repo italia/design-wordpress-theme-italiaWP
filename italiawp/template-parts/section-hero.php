4f7fc02740482823851f2664580d9a591e845c1f
Boris <ilvecchiobob@gmail.com>
2018-11-05 10:42:42 +0100

    update - ver 1.1.7
    - Il bottone "Vai alle notizie" della sezione "Hero" scompare se in home non è attiva la sezione "Ultime news"
    - Bugfix sulle "Card"

--- italiawp/template-parts/section-hero.php
+++ italiawp/template-parts/section-hero.php
@@ -31,8 +31,10 @@ if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->t
         </p>
         <h2 class="u-text-h2"><a href="<?php the_permalink(); ?>" class="u-color-95 u-textClean"><?php the_title(); ?></a></h2>
         <p class="u-padding-r-bottom u-padding-r-top u-text-p u-margin-r-bottom"><?php echo(get_the_excerpt()); ?></p>
+        <?php if (get_theme_mod('active_section_last_news')) { ?>
         <a href="#news" class="js-scrollTo u-linkClean Button Button--info u-text-r-xs">Vai alle notizie
             <span class="u-padding-r-left Icon Icon-expand"></span></a>
+        <?php } ?>
     </div>
     <div class="Forward Forward--absolute" aria-hidden="true">
         <span class="Icon Icon-expand"></span>

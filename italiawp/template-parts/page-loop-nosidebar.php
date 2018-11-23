90b46831cdfaf20f8093a5897c72bc852ced8ff2
Boris <ilvecchiobob@gmail.com>
2018-11-23 13:10:12 +0100

    update - ver 1.1.9
    - Aggiunto il template "Senza Sidebar" per le pagine, in modo da scegliere su quali pagine disattivare la sidebar

---
+++ italiawp/template-parts/page-loop-nosidebar.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * ### LOOP PAGINA SENZA SIDEBAR ###
+ *
+ */
+?>
+
+<?php
+
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<div class="u-layout-wide u-layoutCenter u-text-r-l u-padding-r-top u-margin-r-bottom u-layout-r-withGutter">
+    
+    <h1 class="u-text-h2"><?php the_title(); ?></h1>
+    
+    <section class="Grid Grid--withGutter">
+
+        <div class="Grid-cell u-md-size8of12 u-lg-size8of12">
+            <div class="Prose u-layout-prose">
+                <?php the_content(); ?>
+                <?php if (get_theme_mod('active_allegati_contenuto'))
+                        get_template_part('template-parts/attachments'); ?>
+            </div>
+        </div>
+        
+    </section>
+
+</div>
+
+<?php endwhile;
+      else : include('error.php');
+      endif; ?>

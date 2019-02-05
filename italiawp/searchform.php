ec204b585a216474dccee47aae80e90501373845
Boris <ilvecchiobob@gmail.com>
2019-02-05 17:24:10 +0100

    update - ver 1.3.1
    - Bugfix su show/hide barra ricerca in risoluzione mobile

--- italiawp/searchform.php
+++ italiawp/searchform.php
@@ -1,4 +1,4 @@
-<div class="Header-search italiawp-search">
+<div class="Header-search italiawp-search" id="header-search">
     <form class="Form" method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
         <div class="Form-field Form-field--withPlaceholder Grid u-background-white u-color-grey-30 u-borderRadius-s" role="search">
             <input class="Form-input Form-input--ultraLean Grid-cell u-sizeFill u-text-r-s u-color-black u-text-r-xs u-borderRadius-s"

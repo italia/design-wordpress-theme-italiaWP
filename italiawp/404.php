373d158e823671e14a69f8fb80b2200ac4b616d5
Emanuele <emanuele.gurini89@gmail.com>
2019-10-17 16:55:34 +0200

    php
    modifica

--- italiawp/404.php
+++ italiawp/404.php
@@ -7,6 +7,7 @@
             <h2 class="ErrorPage-subtitle">Pagina non trovata</h2>
             <p class="Prose u-margin-r-all">Oops! La pagina che cerchi non è stata trovata, <a href="javascript:history.back();" title="Torna alla pagina precedente">torna indietro</a> o utilizza il menu per continuare la navigazione.</p>
             <a class="Button Button--default u-margin-r-all" href="<?php echo esc_url(home_url('/')); ?>">Torna alla Home</a>
+            <h1>hello world<h1>        
         </div>
 
     </div>
